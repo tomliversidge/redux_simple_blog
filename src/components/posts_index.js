dff50e475026967cd6067a465a8b42f4361d1f1d
Tom Liversidge <tliversidge@roishoppermarketing.com>
2016-03-14 20:37:55 +0000

    Ready to call API

---
+++ src/components/posts_index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+  componentWillMount(){
+    // only called once
+    console.log("fetch data here");
+  }
+
+  render() {
+    return (
+        <div></div>
+    );
+  }
+}
+
+export default PostsIndex;
